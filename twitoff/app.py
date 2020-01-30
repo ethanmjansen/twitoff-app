c4e52730b06f1f6e82bb22b5a317036e87d31f12
ethanmjansen <ethanm.jansen@gmail.com>
2020-01-29 17:00:29 -0700

    Increased functionality

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,18 +1,40 @@
 from decouple import config
-from dotenv import load_dotenv
-from flask import Flask, render_template
-from .models import DB, User
+from flask import Flask, render_template, request
 
-load_dotenv()
+from .models import DB, User
+from .twitter import add_or_update_user
+
 
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return render_template('base.html', title='the great Twitter twitoff!', users=User.query.all())
+        DB.create_all()
+        return render_template('base.html', title='Home', users=User.query.all())
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['username']
+        try:
+            if request.method == "POST":
+                add_or_update_user(name)
+                message = f'User {name} successfully added!'
+            tweets = User.query.filter(User.name==name).one().tweets
+        except Exception as e:
+            message = f'Error while try to add user {name}: {e}'
+            tweets = []
+        return render_template('user.html', title=name, message=message, tweets=tweets)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
 
     return app
